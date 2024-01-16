3094771dc5039bf62b492446a3f5889ac05f0947
trần thế anh <trantheanh132@gmail.com>
2024-01-16 20:09:59 +0700

    [MODIFIED]: Modified project Packages and add layer dependecy injections in API

--- TAT.Ecommerce.API/Program.cs
+++ TAT.Ecommerce.API/Program.cs
@@ -1,33 +1,44 @@
-namespace ChainStoresManagement.API
+
+using TAT.Ecommerce.Domain.DI;
+using TAT.Ecommerce.Infrastructure.DI;
+
+
+namespace TAT.Ecommerce.API
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+            //ConfigureServices
+            _ = builder.Services.AddDomainLayer();
+            _ = builder.Services.AddInfrastructureLayer(builder.Configuration);
+
+
 
             // Add services to the container.
 
-            builder.Services.AddControllers();
+            _ = builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            _ = builder.Services.AddEndpointsApiExplorer();
+            _ = builder.Services.AddSwaggerGen();
 
-            var app = builder.Build();
+
+            WebApplication app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
+                _ = app.UseSwagger();
+                _ = app.UseSwaggerUI();
             }
 
-            app.UseHttpsRedirection();
+            _ = app.UseHttpsRedirection();
 
-            app.UseAuthorization();
+            _ = app.UseAuthorization();
 
 
-            app.MapControllers();
+            _ = app.MapControllers();
 
             app.Run();
         }
